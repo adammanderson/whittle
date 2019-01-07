f9b51dfd08a64fc24c2a0243dcdb7a6741248fcf
Adam Anderson <adam.m.anderson@gmail.com>
2019-01-07 09:24:14 +0000

    Lint files

--- src/electron-wait-react.js
+++ src/electron-wait-react.js
@@ -1,24 +1,23 @@
-const net = require('net');
-const port = process.env.PORT ? (process.env.PORT - 100) : 3000;
+const net = require('net')
+const { exec } = require('child_process')
 
-process.env.ELECTRON_START_URL = `http://localhost:${port}`;
+const port = process.env.PORT ? (process.env.PORT - 100) : 3000
 
-const client = new net.Socket();
+process.env.ELECTRON_START_URL = `http://localhost:${port}`
 
-let startedElectron = false;
-const tryConnection = () => client.connect({port: port}, () => {
-        client.end();
-        if(!startedElectron) {
-            console.log('starting electron');
-            startedElectron = true;
-            const exec = require('child_process').exec;
-            exec('npm run electron');
-        }
-    }
-);
+const client = new net.Socket()
 
-tryConnection();
+let startedElectron = false
+const tryConnection = () => client.connect({ port }, () => {
+  client.end()
+  if (!startedElectron) {
+    startedElectron = true
+    exec('npm run electron')
+  }
+})
 
-client.on('error', (error) => {
-    setTimeout(tryConnection, 1000);
-});
+tryConnection()
+
+client.on('error', () => {
+  setTimeout(tryConnection, 1000)
+})
